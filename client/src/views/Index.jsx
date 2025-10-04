48cecbc58b6e22c8df594ee7c964394bfb377865
cruzferrer <cruzricardofp@gmail.com>
2025-10-04 14:48:59 -0600

    checkpoint

--- client/src/views/Index.jsx
+++ client/src/views/Index.jsx
@@ -8,8 +8,9 @@ const Index = () => {
         
         <div className="relative w-full h-screen overflow-hidden">
             <StarsCanvas />
+             <HeaderCanvas />
             <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[600px] h-[600px]">
-                <HeaderCanvas />
+               
                 <EarthCanvas />
             </div>
         </div>
